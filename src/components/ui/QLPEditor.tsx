0cbf06762ab1ee28d458d9716ba7dfbedb53b06a
JaferShar <jafer.shar@gmail.com>
2025-05-02 17:07:51 +0200

    added ui components for language selector

--- src/components/ui/QLPEditor.tsx
+++ src/components/ui/QLPEditor.tsx
@@ -1,15 +1,18 @@
 import {Editor} from "@monaco-editor/react";
+import {useState} from "react";
 
 function QLPEditor() {
-    return (
-        <div>
-            <Editor
-                height="90vh"
-                defaultLanguage="javascript"
-                defaultValue="// some comment"
-                theme="vs-dark"/>
-        </div>
-    );
+    const [value, setValue] = useState('');
+    return <div>
+        <Editor
+            height="90vh"
+            defaultLanguage="javascript"
+            defaultValue="// Some Quantum Code"
+            theme="vs-dark"
+            value={value}
+            onChange={(value) => setValue(value || '')}
+        />
+    </div>;
 }
 
 export default QLPEditor;
