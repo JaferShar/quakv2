6cb54dd486a109df86daf315eaf48698e7319ff2
JaferShar <jafer.shar@gmail.com>
2025-05-04 16:41:29 +0200

    added a margin to the bottom of the language selector

--- src/components/ui/LanguageSelector.tsx
+++ src/components/ui/LanguageSelector.tsx
@@ -60,7 +60,7 @@ export function LanguageSelector() {
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 mb-2">
                 <FormField
                     control={form.control}
                     name="language"
